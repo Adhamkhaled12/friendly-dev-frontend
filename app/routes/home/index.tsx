41fe2fbb9c37f4e0d59d311e82fc5883508e544d
Adham <adhambackshish12@gmail.com>
2025-12-28 20:28:04 +0200

    Modify fetching data from JSON Server to Strapi

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,8 +1,8 @@
-import type { Project } from "~/types";
+import type { Project, StrapiPost, StrapiProject, StrapiResponse } from "~/types";
 import type { Route } from "./+types/index";
 import FeaturedProjects from "~/components/FeaturedProjects";
 import AboutPreview from "~/components/AboutPreview";
-import type { PostMeta } from "~/types";
+import type { Post } from "~/types";
 import LatestPosts from "~/components/LatestPost";
 
 
@@ -13,24 +13,42 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export async function loader({request}: Route.LoaderArgs): Promise<{projects: Project[], posts: PostMeta[]}> {
+export async function loader({request}: Route.LoaderArgs): Promise<{projects: Project[], posts: Post[]}> {
   const url = new URL(request.url);
 
   const [projectRes, postRes] = await Promise.all([
-    fetch(`${import.meta.env.VITE_API_URL}/projects`),
-    fetch(new URL('/posts-meta.json', url))
+    fetch(`${import.meta.env.VITE_API_URL}/projects?filters[featured][$eq]=true&populate=*`),
+    fetch(`${import.meta.env.VITE_API_URL}/posts?sort[0]=date:desc&populate=*`)
   ]);
   
   if(!projectRes.ok || !postRes.ok){
     throw new Error('Failed to fetch projects or posts');
   }
 
-  const [projects, posts] = await Promise.all([
-    projectRes.json(),
-    postRes.json()
-  ])
+  const projectJson: StrapiResponse<StrapiProject> = await projectRes.json();
+  const postJson: StrapiResponse<StrapiPost> = await postRes.json();
 
-  console.log(projects, posts);
+  const projects = projectJson.data.map((item) => ({
+    id: item.id,
+    documentId: item.documentId,
+    title: item.title,
+    description: item.description,
+    image: item.image?.url ? `${item.image.url}` : '/images/no-image.png',
+    url: item.url,
+    date: item.date,
+    category: item.category,
+    featured: item.featured
+  }));
+
+  const posts = postJson.data.map((item) => ({
+    id: item.id,
+    title: item.title,
+    slug: item.slug,
+    excerpt: item.excerpt,
+    body: item.body,
+    image: item.image?.url ? `${item.image.url}` : '/images/no-image.png',
+    date: item.date,
+  }));
   
   return {projects, posts};
 }
--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/index";
-import type { Project } from "~/types";
+import type { Project, StrapiProject, StrapiResponse } from "~/types";
 import ProjectCard from "~/components/ProjectCard";
 import { useState } from "react";
 import Pagination from "~/components/Pagination";
@@ -7,10 +7,22 @@ import { AnimatePresence, motion } from "framer-motion";
 
 // Before the page renders, go get the data it needs as React Router calls loader before rendering ProjectsPage whatever you return from loader becomes page data
 export async function loader({request}: Route.LoaderArgs): Promise<{projects: Project[]}> {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
-  const data = await res.json();
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects?populate=*`);
+  const json: StrapiResponse<StrapiProject> = await res.json();
 
-  return {projects: data};
+  const projects = json.data.map((item) => ({
+    id: item.id,
+    documentId: item.documentId,
+    title: item.title,
+    description: item.description,
+    image: item.image?.url ? `${item.image.url}` : '/images/no-image.png',
+    url: item.url,
+    date: item.date,
+    category: item.category,
+    featured: item.featured
+  }))
+
+  return {projects};
 }
 
 // Destructure the data returned from the loader, so loaderData is an object that has inside it the projects
